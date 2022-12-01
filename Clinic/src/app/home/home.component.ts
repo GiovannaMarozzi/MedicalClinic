1ba43381ddc615565d14a883b8210f71d792b5c0
GiovannaMarozzi <giovanna.machado99@hotmail.com>
2022-12-01 12:58:09 -0300

    Finalizing page home

--- Clinic/src/app/home/home.component.ts
+++ Clinic/src/app/home/home.component.ts
@@ -3,17 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   // templateUrl: './home.component.html',
-
   template:`
-  <section class="hero is-primary is-bold is full-heigth">
+  <section class="hero is-primary is-bold is-fullheight">
     <div class="hero-body">
-      <p class="title">
-        Home Page
-      </p>
+      <div class="container has-text-centered">
+      <button type="button" class="btn btn-primary btn-lg btn-block">Sistema de Médicos</button>
+      <button type="button" class="btn btn-secondary btn-lg btn-block">Sistema de Pacientes</button>
+      </div>
     </div>
   </section>`
     ,
-  styles:[]
+  styles:[`.hero{
+    background-image: url('/assets/doctor.jpeg')!important;
+    background-size: cover;
+    background-position: center center;
+  }`]
  
 })
 export class HomeComponent {
