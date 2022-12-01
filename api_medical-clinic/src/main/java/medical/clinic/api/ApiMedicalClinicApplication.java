90c0fbfc1018bf5b59f58a08cc1d9115722e58c3
GiovannaMarozzi <giovanna.machado99@hotmail.com>
2022-11-30 22:32:50 -0300

    Initializing the backEnd part already with the maven dependencies

---
+++ api_medical-clinic/src/main/java/medical/clinic/api/ApiMedicalClinicApplication.java
@@ -0,0 +1,13 @@
+package medical.clinic.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMedicalClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMedicalClinicApplication.class, args);
+	}
+
+}
