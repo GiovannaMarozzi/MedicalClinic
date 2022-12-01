096e85e5955a896bb256091111b92fa39c3de9de
GiovannaMarozzi <giovanna.machado99@hotmail.com>
2022-12-01 11:58:13 -0300

    Add framework bulma

--- Clinic/src/app/home/home.component.ts
+++ Clinic/src/app/home/home.component.ts
@@ -2,8 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  // templateUrl: './home.component.html',
+
+  template:`
+  <section class="hero is-primary is-bold is full-heigth">
+    <div class="hero-body">
+      <p class="title">
+        Home Page
+      </p>
+    </div>
+  </section>`
+    ,
+  styles:[]
+ 
 })
 export class HomeComponent {
 
