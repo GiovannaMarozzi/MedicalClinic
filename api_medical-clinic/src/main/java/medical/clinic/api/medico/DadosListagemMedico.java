dafa6b8d95fe60387185361c5efb4692f4aaa769
GiovannaMarozzi <giovanna.machado99@hotmail.com>
2022-12-02 11:44:26 -0300

    Endpoint Get, Put and Delete

--- api_medical-clinic/src/main/java/medical/clinic/api/medico/DadosListagemMedico.java
+++ api_medical-clinic/src/main/java/medical/clinic/api/medico/DadosListagemMedico.java
@@ -1,8 +1,8 @@
 package medical.clinic.api.medico;
 
 public record DadosListagemMedico(Long id, String nome, String email, String crm, Especialidade especialidade) {
-//    public DadosListagemMedicos(Medico medico){
-//        this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade()); //Está sendo chamado o próprio constutor do método para que possa levar os parâmetros necessários
-//    }
+    public DadosListagemMedico(Medico medico){
+        this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade()); //Está sendo chamado o próprio constutor do método para que possa levar os parâmetros necessários
+    }
 
 }
