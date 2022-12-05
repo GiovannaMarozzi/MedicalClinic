d9ecc92e7d4c1e9ab7ddc84fdac9bd4476893223
GiovannaMarozzi <giovanna.machado99@hotmail.com>
2022-12-05 13:25:15 -0300

    Implement informations in table

--- Clinic/src/app/form-doctors/form-doctors.component.ts
+++ Clinic/src/app/form-doctors/form-doctors.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Doctors } from './../doctors';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-doctors',
   templateUrl: './form-doctors.component.html',
   styleUrls: ['./form-doctors.component.css']
 })
-export class FormDoctorsComponent {
-
+export class FormDoctorsComponent implements OnInit {
+    doctor: Doctors[] = [];
+    constructor(){}
+    ngOnInit(): void{
+      this.doctor =[{
+        "id":1,
+        "nome": "Giovanna",
+        "email": "fyufryfhr@fgruyfghrtfgr",
+        "crm":"2565484",
+        "especialidade": "teste"
+      },
+      {"id":2,
+      "nome": "Teste2",
+      "email": "fyufryfhr@fgruyfghrtfgr",
+      "crm":"defrfr545845",
+      "especialidade": "teste2"
+    }
+      
+    ];
+    }
 }
