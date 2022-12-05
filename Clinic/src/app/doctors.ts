e037011e15714d5281b955717ebffc49d2d13a53
GiovannaMarozzi <giovanna.machado99@hotmail.com>
2022-12-05 13:24:25 -0300

    Implement informations in table

---
+++ Clinic/src/app/doctors.ts
@@ -0,0 +1,14 @@
+export class Doctors {
+    id!: number;
+    nome!: string;
+    email!: string;
+    especialidade!: string;
+    crm!: string;
+    // telefone!: string;
+    // logradouro!: string;
+    // numero!: string;
+    // complemento!: string;
+    // cidade!: string;
+    // uf!: string;
+    // cep!: string;
+}
