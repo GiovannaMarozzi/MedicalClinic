87dce3b720483692624f392270d20a3d43ac2c01
GiovannaMarozzi <36972799+GiovannaMarozzi@users.noreply.github.com>
2022-12-12 21:40:05 -0300

    Delete Clinic directory

--- Clinic/src/app/model/patients/patientForm.ts
+++
@@ -1,9 +0,0 @@
-import { EnderecoForm } from "../endereco/enderecoForm";
-
-export class PatientForm{
-   cpf!: string; 
-   nome!: string;
-   email!: string;
-   telefone!: string;
-   endereco!: Array<EnderecoForm>;
-}
