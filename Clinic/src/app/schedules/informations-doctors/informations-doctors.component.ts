59a41b850199e16b4b9531fd8258774759ba6491
GiovannaMarozzi <giovanna.machado99@hotmail.com>
2022-12-18 11:54:08 -0300

    Modal

---
+++ Clinic/src/app/schedules/informations-doctors/informations-doctors.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-informations-doctors',
+  templateUrl: './informations-doctors.component.html',
+  styleUrls: ['./informations-doctors.component.css']
+})
+export class InformationsDoctorsComponent implements OnInit {
+  
+  constructor(){}
+  
+  ngOnInit(): void {
+   
+  }
+
+}
